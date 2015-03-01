3284fe43568472a0ace67e83a8658275f68205d7
Elendil <anicet.reglat@gmail.com>
2015-03-01 02:13:44 +0100

    Debug du système de cache

--- system/cache.php
+++ system/cache.php
@@ -1,6 +1,7 @@
 <?php
 namespace system;
 use system\Config;
+use system\Log;
 
 class Cache {
 
@@ -10,13 +11,20 @@ class Cache {
 
     /**
      * @param $path Chemin complémentaire du cache
-     * @param int $time Temps à cacher
+     * @param int $time Temps à cacher en minutes
      */
     function __construct($path, $time = 10)
     {
-        $this->path = DS.APP.'cache'.DS.$path;
-        $this->time = $time;
-        $this->activated = Config::$application['enable_cache'];
+
+        $this->path = APP.'cache'.DS.$path.DS;
+        if(!is_dir($this->path)){
+            if(mkdir($this->path, 0755))
+                Logs::write('Cache','Info', 'Le dossier '.$this->path.' a bien été créé');
+            else
+                Logs::write('Cache','Erreur', 'Le dossier '.$this->path.' n\'a pas pu être créé');
+        }
+        $this->time = $time*60;
+        $this->activated = Config::$app['enable_cache'];
     }
 
     /**
@@ -27,7 +35,10 @@ class Cache {
     public function set($filename, Array $content)
     {
         if($this->activated)
-            file_put_contents($this->path.$filename.'.json', json_encode($content));
+            if(file_put_contents($this->path.$filename.'.json', json_encode($content)))
+                Logs::write('Cache','Info', 'Le cache '.$filename.'.json a bien été créé');
+            else
+                Logs::write('Cache','Erreur', 'Le cache '.$filename.'.json n\'a pas pu être créé');
     }
 
     /**
@@ -40,7 +51,7 @@ class Cache {
         if($this->activated){
             $filename = $this->path.$filename.'.json';
             if($this->cached($filename))
-                return json_decode(file_get_contents($filename));
+                return json_decode(file_get_contents($filename), true);
             return false;
         }
         return false;
