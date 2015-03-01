69f6f77da1a0d1008a95815c468e877039a3874e
Elendil <anicet.reglat@gmail.com>
2015-03-01 16:43:24 +0100

    Amélioration du générateur de requêtes de type SELECT

--- system/querys.php
+++ system/querys.php
@@ -22,13 +22,58 @@ class Querys {
 
     /**
      * SELECT dans la base de donnée
-     * @param $table Nom de la table
-     * @param array $toSelect Un array de la liste des champs à récupérér
+     * @param array $toSelect Un array avec ce qu'on veut dedans... <o/
      * @return mixed
      */
-    protected function select($table, Array $toSelect){
-        $columns = implode(", ", $toSelect);
-        $stmt = $this->pdo->prepare('SELECT '.$columns.' FROM '.$table);
+    protected function select(Array $req){
+        $sql = 'SELECT ';
+        if(isset($req['fields'])){
+            if(is_array($req['fields'])){
+                $sql .= implode(', ', $req['fields']);
+            }
+            else{
+                $sql .= $req['fields'];
+            }
+        }
+        else{
+            $sql .= "*";
+        }
+
+        if(isset($req['table'])){
+            $sql .= " FROM ".$req['table'];
+        }
+        else{
+             return false;
+        }
+
+        if(isset($req['where'])){
+            $sql .= " WHERE ";
+            if(!is_array($req['where'])){
+                $sql .= $req['where'];
+            }
+            else{
+                $where = [];
+                foreach($req['where'] as $field => $value){
+                    if(is_array($value)){
+                        if(!is_numeric($value[1])){
+                            $value[1] = '"'.mysql_escape_string($value[1]).'"';
+                        }
+                        $where[] = $field.' '.$value[0].' '.$value[1];
+                        break;
+                    }
+                    elseif(!is_numeric($value)){
+                        $value = '"'.mysql_escape_string($value).'"';
+                        $where[] = $field.' LIKE '.$value;
+                    }
+                    else{
+                        $where[] = $field.' = '.$value;
+                    }
+                }
+                $sql .= implode(' AND ', $where);
+            }
+        }
+
+        $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
